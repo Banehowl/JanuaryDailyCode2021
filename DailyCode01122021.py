0fb86c37df3d0bdcaab03ba12ba0fbd0c435c489
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-01-12 09:06:35 -0800

    Modules and Packages (quick look)
    Need to find a more in-depth look into this one, but a quick look at modules and packages

---
+++ DailyCode01122021.py
@@ -0,0 +1,48 @@
+# ------------------------------------------------------
+#	Daily Code	01/12/2021
+#   "Modules and Packages" Lesson from learnpython.org
+#   Coded by: Banehowl
+# ------------------------------------------------------
+
+# A Module is a peice of software that has a specific fuctionality. For example, when building a ping pong
+# game, one module would be responsible for the game logic, and another module would be responsible for
+# drawing the game on the screen. Each module is a different file, which can be edited seperately.
+
+# Writing modules
+# Modules in Python is simply python files with the .py extension. The name of the module will be the
+# name of the file. A Python module can have a set of fuctions, classes, or variables defined and
+# implemented
+
+# In the ping pong example above, the files would be organized below:
+# mygame/
+# mygame/game.py
+# mygame/draw.py
+
+# Modules are imported from other modules using the 'import' command. In this example, the game.py
+# script may look something like this:
+
+# # game.py
+# # import the draw module
+# import draw
+#
+# def play_game()
+#     ...
+#
+# def main()
+#     result = play_game()
+#     draw.draw_game(result)
+#
+# # this means that if this script is executed, then main() will be executed
+# if _name_ == '_main_';:
+#    main()
+
+# The draw module may look something like this:
+
+# # draw.py
+#
+# def draw_game()
+#     ...
+#
+# def clear_screen(screen)
+#     ...
+
