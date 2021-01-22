f497938465868aec1ff8dc6b4434c88204a885ad
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-01-22 08:40:25 -0800

    Code Introspection
    Daily code lesson 01/22/2021 about available code introspection command and example.

---
+++ DailyCode012322021.py
@@ -0,0 +1,38 @@
+# ----------------------------------------------------
+#	Daily Code	01/22/2021
+#   "Code Introspection" Lesson from learnpython.org
+#   Coded by: Banehowl
+# ----------------------------------------------------
+
+# Code introspection is the ability to examine classes, functions and keywords to know what they are,
+# what they do and what they know.
+
+# Python provides several functions and utilities for code introspection.
+
+# help()
+# dir()
+# hasattr()
+# id()
+# type()
+# repr()
+# callable()
+# issubclass()
+# isinstance()
+# __doc__
+# __name__
+
+# Exercise
+# Print a list of all attributes of the given Vehicle object.
+
+# Define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+# Print a list of all attributes of the Vehicle class.
+print(dir(Vehicle))
