243053b5eabcb7271cd1abd3fc74a1b4856a2240
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-01-26 08:43:24 -0800

    Exercise 1 from edabit.com
    Exercise 1 from edabit.com - Return the Sum of Two Numbers

---
+++ DailyCode01262021.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------
+#	Daily Code	01/26/2021
+#   "Return the Sum of Two Numbers" Lesson from edabit.com
+#   Coded by: Banehowl
+# ----------------------------------------------------------
+
+# Return the Sum of Two Numbers
+# addition(3, 2) -> 5
+# addition(-3, -6) -> -9
+# addition(7, 3) -> 10
+
+def addition(a, b):
+    solution = a + b
+    print(solution) # return(solution) is the solution on the website
+
+addition(3, 2)
+addition(-3, -6)
+addition(7, 3)
