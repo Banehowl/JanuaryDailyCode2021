613b8c4cfdb2bed2dd53cf1f4d7ed4f46319ebc6
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-01-27 09:04:24 -0800

    Return the Next Number from the Interger Passed
    Lesson from edabit.com

---
+++ DailyCode01272021.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+#	Daily Code	01/27/2021
+#   "Return the Next Number from the Interger Passed" Lesson from edabit.com
+#   Coded by: Banehowl
+# ----------------------------------------------------------------------------
+
+# Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+def addition(num):
+    num = num + 1
+    return(num)
