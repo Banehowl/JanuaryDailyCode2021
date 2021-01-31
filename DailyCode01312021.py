437c359eada486534eb3f7043531fb84cae2a07d
Banehowl <76931164+Banehowl@users.noreply.github.com>
2021-01-31 07:55:12 -0800

    Fix the Code (Part 1)
    Daily Code (and last of January 2021) - fix the syntax error code.

---
+++ DailyCode01312021.py
@@ -0,0 +1,19 @@
+# ------------------------------------------------
+#	Daily Code	01/31/2021
+#   "Buggy Code (Part 1)" Lesson from edabit.com
+#   Coded by: Banehowl
+# ------------------------------------------------
+
+# Fix the code in the code tab to pass this challenge
+
+# def cubes(a):
+# 	retunr a ** 3
+
+# Solution
+def cubes(a):
+    return a ** 3
+
+# Check
+print cubes(3)
+print cubes(5)
+print cubes(10)
